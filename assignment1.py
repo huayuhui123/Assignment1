3066708f630cdb2d69e566024df6d300d46d047b
yuhui Hua <2826537460@qq.com>
2021-10-07 23:09:38 +0800

    Design the rough model of menu,not complete its functions

--- assignment1.py
+++ assignment1.py
@@ -1,12 +1,32 @@
 """
 Update this module docstring with your own details
 Name:YuhuiHua
-Date started:
+Date started:2021.10.7
 """
-
-
+menu="""Menu:
+L - List all albums
+A - Add new album
+M - Mark an album as completed
+Q - Quit"""
+def listout():
+def add():
+def mark():
+def mainmenu():
+    print(menu)
+    choice = input(">>>").upper()
+    while choice!="Q"or"4":
+        if choice=="L"or"1":
+            listout()
+        elif choice=="A"or"2":
+            add()
+        elif choice=="M"or"3":
+            mark()
+        else:
+            print("Invalid menu choice")
+        print(menu)
+        choice = input(">>>").upper()
+    print("{}albums saved to albums.csv".format())
 def main():
-    print("Album Tracker 1.0 - by <Your Name>")
-
-
+    print("Album Tracker 1.1 - by Yuhui Hua")
+    mainmenu()
 main()
